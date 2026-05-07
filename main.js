e3bc0aec4485a94ca9b6b70c4cb91dc4c5f1c5e9
YarlinRivera <yarlinrivera12@gmail.com>
2026-05-07 00:20:09 -0500

    Add footer.html and header.html with mian.js to optimize the website

---
+++ main.js
@@ -0,0 +1,12 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+});
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+        document.getElementById('anio-footer').innerHTML = new Date().getFullYear();
+});
