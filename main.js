5e96da761c8cd82135a9154f70b1ac47f10c6d69
YarlinRivera <yarlinrivera12@gmail.com>
2026-05-08 17:26:42 -0500

    fix menu navegacion from the header and fix vulnerability from innerHTML

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 fetch('header.html')
     .then(response => response.text())
     .then(data => {
-        document.getElementById('header-container').innerHTML = data;
-});
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        document.getElementById('header-container').appendChild(doc.body.firstChild);});
 
 fetch('footer.html')
     .then(response => response.text())
     .then(data => {
-        document.getElementById('footer-container').innerHTML = data;
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        document.getElementById('footer-container').appendChild(doc.body.firstChild);
         document.getElementById('anio-footer').innerHTML = new Date().getFullYear();
 });
