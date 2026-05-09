dadc3201c4c8157ad6bcaaaeb13186cb37138b67
YarlinRivera <yarlinrivera12@gmail.com>
2026-05-08 22:54:00 -0500

    fix areas de invetigacion distribution

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
-        document.getElementById('header-container').appendChild(doc.body.firstChild);});
+fetch("header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+    document
+      .getElementById("header-container")
+      .appendChild(doc.body.firstChild);
+  });
 
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
-        document.getElementById('footer-container').appendChild(doc.body.firstChild);
-        document.getElementById('anio-footer').innerHTML = new Date().getFullYear();
-});
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+    document
+      .getElementById("footer-container")
+      .appendChild(doc.body.firstChild);
+    document.getElementById("anio-footer").innerHTML = new Date().getFullYear();
+  });
